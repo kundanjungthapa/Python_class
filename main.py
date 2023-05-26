61651c54da5c2a757bf761f468ffc0c683a634ba
npaudyal321 <naween321@gmail.com>
2023-05-26 07:07:04 +0545

    tkinter setup

--- main.py
+++ main.py
@@ -1,25 +1,101 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-from env import *
+import tkinter
+from tkinter import ttk
+from tkinter import messagebox
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def submit_data():
+    status = terms_check_var.get()
+    if status == "Accepted":
+        first_name = first_name_entry.get()
+        last_name = last_name_entry.get()
+        title = title_combobox.get()
+        age = age_spinbox.get()
+        nationality = nationality_combobox.get()
+
+        num_courses = num_courses_spinbox.get()
+        num_semesters = num_semesters_spinbox.get()
+        registration_status = reg_status_var.get()
+        print(first_name)
+    else:
+        tkinter.messagebox.showwarning(title="Error", message="You have not accepted the terms and conditions")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+window = tkinter.Tk()
+window.title("Dta Entry Form")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+frame = tkinter.Frame(window)
+frame.pack()
 
-DATABASE_NAME = DATABASE_NAME
-DATABASE_USER = DATABASE_USER
-PASSWORD = PASSWORD
-HOST = HOST
-PORT = PORT
+# Saving user info
+user_info_frame = tkinter.LabelFrame(frame, text="User Information")
+user_info_frame.grid(row=0, column=0, padx=20, pady=10)
 
+first_name_label = tkinter.Label(user_info_frame, text="First Name")
+first_name_label.grid(row=0, column=0)
+last_name_label = tkinter.Label(user_info_frame, text="Last Name")
+last_name_label.grid(row=0, column=1)
+
+first_name_entry = tkinter.Entry(user_info_frame)
+last_name_entry = tkinter.Entry(user_info_frame)
+first_name_entry.grid(row=1, column=0)
+last_name_entry.grid(row=1, column=1)
+
+title_label = tkinter.Label(user_info_frame, text="Title")
+title_combobox = ttk.Combobox(user_info_frame, values=["", "Mr.", "Ms."])
+title_label.grid(row=0, column=2)
+title_combobox.grid(row=1, column=2)
+
+age_label = tkinter.Label(user_info_frame, text="Age")
+age_spinbox = tkinter.Spinbox(user_info_frame, from_=18, to=100)
+age_label.grid(row=2, column=0)
+age_spinbox.grid(row=3, column=0)
+
+
+nationality_label = tkinter.Label(user_info_frame, text="Nationality")
+nationality_combobox = ttk.Combobox(user_info_frame, values=["Nepal", "Singapore", "Japan"])
+nationality_label.grid(row=2, column=1)
+nationality_combobox.grid(row=3, column=1)
+
+for widget in user_info_frame.winfo_children():
+    widget.grid_configure(padx=10, pady=5)
+
+
+# Saving Course Info
+courses_frame = tkinter.LabelFrame(frame)
+courses_frame.grid(row=1, column=0, sticky="news", padx=20, pady=10)
+registered_label = tkinter.Label(courses_frame, text="Registration Status")
+reg_status_var = tkinter.StringVar(value="Not Registered")
+
+registered_check = tkinter.Checkbutton(courses_frame, text="Currently Registered",
+                                       variable=reg_status_var, onvalue="Registered", offvalue="Not Registered")
+registered_label.grid(row=0, column=0)
+registered_check.grid(row=1, column=0)
+
+num_courses_label = tkinter.Label(courses_frame, text="# Completed Courses")
+num_courses_spinbox = tkinter.Spinbox(courses_frame, from_=0, to='infinity')
+num_courses_label.grid(row=0, column=1)
+num_courses_spinbox.grid(row=1, column=1)
+
+num_semesters_label = tkinter.Label(courses_frame, text="# Semesters")
+num_semesters_spinbox = tkinter.Spinbox(courses_frame, from_=0, to='infinity')
+num_semesters_label.grid(row=0, column=2)
+num_semesters_spinbox.grid(row=1, column=2)
+
+for widget in courses_frame.winfo_children():
+    widget.grid_configure(padx=10, pady=5)
+
+
+# Accept terms
+terms_frame = tkinter.LabelFrame(frame, text="Terms & Conditions")
+terms_frame.grid(row=2, column=0, sticky="news", padx=20, pady=10)
+
+terms_check_var = tkinter.StringVar(value="Not Accepted")
+terms_check = tkinter.Checkbutton(terms_frame, text="I accept the terms and conditions",
+                                  variable=terms_check_var, onvalue="Accepted", offvalue="Not Accepted")
+terms_check.grid(row=0, column=0)
+
+# Button
+button = tkinter.Button(frame, text="Submit", command=submit_data)
+button.grid(row=3, column=0, sticky="news", padx=20, pady=10)
+
+window.mainloop()
